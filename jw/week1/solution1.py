0047c626592ab3109395d2b7b8fcd1f29cc1df79
Donghyun <pointehd@naver.com>
2023-05-04 13:37:36 +0900

    Feat: 백준 제곱수의 합

---
+++ jw/week1/solution1.py
@@ -0,0 +1,41 @@
+# 제곱수의 합
+# https://www.acmicpc.net/problem/1699
+
+# 해설 : https://lakelouise.tistory.com/61
+# RunTime :
+# Memory :
+
+n = int(input())
+
+dp = [i for i in range(n + 1)]
+
+for i in range(1, n + 1):
+    for j in range(1, i):
+        if j * j > i:
+            break
+        if dp[i] > dp[i - j * j] + 1:
+            dp[i] = dp[i - j * j] + 1
+# print(dp)
+
+print(dp[n])
+
+
+# ==================
+
+
+n = int(input())
+
+dp = [i for i in range(n + 1)]
+
+tmp = 1
+while tmp**2 < n + 1:
+    dp[tmp**2] = 1
+    tmp += 1
+
+for i in range(1, n + 1):
+    for j in range(1, i // 2 + 1):
+        if dp[i] == 1:
+            break
+        dp[i] = min(dp[i], dp[i - j] + dp[j])
+
+print(dp[n])
